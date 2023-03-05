4fb928224d1fa3468a06aae7388753244c8f68f4
Mina Makar <minamakar7@gmail.com>
2023-03-05 20:35:33 +0100

    Added more version of model and the extenstion

--- Hatespeech_chrome_extension/hatespeech.js
+++ Hatespeech_chrome_extension/hatespeech.js
@@ -1,58 +1,137 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-let link = "http://bfba-34-69-195-3.ngrok.io/query";
+let link = "http://4722-35-201-212-13.ngrok.io/query";
 let observer = new MutationObserver(retrieveComments);
-let comments_predictions = {};
 
+let comments_predictions = {};
+const config = { subtree: true, childList: true };
 function waitForElement(selector) {
 	return new Promise((resolve) => {
 		if (document.querySelector(selector)) {
 			return resolve(document.querySelector(selector));
 		}
-
 		const observer = new MutationObserver((mutations) => {
 			if (document.querySelector(selector)) {
 				resolve(document.querySelector(selector));
 				observer.disconnect();
 			}
 		});
-
-		observer.observe(document.body, {
-			childList: true,
-			subtree: true,
-		});
+		observer.observe(document.body, config);
 	});
 }
 
 async function addObserverOnTarget() {
-	// setTimeout(addObserverOnTarget, 1000);
-	// let target = document.querySelector('[aria-label="Timeline: Conversation"]');
-	// if (!target) {
-	// 	//The node we need does not exist yet.
-	// 	//Wait 500ms and try again
-	// 	setTimeout(addObserverOnTarget, 50);
-	// 	return;
-	// }
-	// define what trigger the function --> this is may be not okay
 	const target = await waitForElement('[aria-label="Timeline: Conversation"');
-
-	observer.observe(target, {
-		subtree: true,
-		childList: true,
-	});
+	observer.observe(target, config);
 }
 
-function hide_comment(comment_element) {
-	comment_element.style.backgroundColor = "red";
+function togglePopup(id_tweet) {
+	const pop = document.getElementById("popup" + id_tweet);
+	pop.classList.toggle("show");
+}
+
+function createPopup(id_tweet, prediction) {
+	const createDivStats = document.createElement("div");
+	createDivStats.className = "popup";
+	createDivStats.id = "popup" + id_tweet;
+
+	const divCloseButton = document.createElement("div");
+	divCloseButton.className = "close-btn";
+	divCloseButton.id = "1-" + id_tweet;
+	createDivStats.appendChild(divCloseButton);
+
+	createDivStats.appendChild(prediction);
+
+	const createButton = createButtonHide(id_tweet + "btn", "149px", "50px", "right", "Stats", "absolute", "right");
+	createButton.onclick = function (event) {
+		togglePopup(id_tweet);
+	};
+	return [createDivStats, createButton];
+}
+function createButtonHide(id_button, top, right, float, innerText, position, justifyContent) {
+	const createButton = document.createElement("button");
+	createButton.id = id_button;
+	createButton.innerText = innerText;
+	createButton.style.position = position;
+	createButton.style.justifyContent = justifyContent;
+	createButton.style.top = top;
+	createButton.style.right = right;
+	createButton.style.float = float;
+	createButton.style.paddingLeft = "16px";
+	createButton.style.paddingRight = "16px";
+	createButton.style.borderBottomLeftRadius = "9999px";
+	createButton.style.borderBottomRightRadius = "9999px";
+	createButton.style.borderTopLeftRadius = "9999px";
+	createButton.style.borderTopRightRadius = "9999px";
+	createButton.style.fontWeight = "bolder";
+	return createButton;
+}
+
+function hide_comment(nodeTweet, tweetTextElement, bool, id_comment, prediction) {
+	const createButton = createButtonHide(id_comment, "20px", "45px", "right", "Statistics", "fixed", "right");
+	createButton.style.backgroundColor = "rgb(239, 243, 244)";
+	createButton.style.minHeight = "32px";
+
+	const statisticsDiv = document.createElement("div");
+
+	for (const [key, value] of Object.entries(prediction)) {
+		const modelName = document.createElement("p");
+		modelName.textContent = key;
+		const progressParent = document.createElement("div");
+		progressParent.classList.add("progress");
+
+		const progressValue = document.createElement("div");
+		progressValue.textContent = (value * 100).toFixed(2) + "%";
+		progressValue.classList.add("progress-value");
+		progressValue.style.width = 0;
+		progressValue.setAttribute("data-value", value * 100);
+		progressParent.appendChild(progressValue);
+
+		statisticsDiv.appendChild(modelName);
+		statisticsDiv.appendChild(progressParent);
+		//createSpan.innerText = "\n\n" + key + ": " + value;
+	}
+
+	statisticsDiv.style.display = "none";
+
+	createButton.onclick = function (event) {
+		const x = statisticsDiv;
+		if (x.style.display == "none") {
+			x.style.display = "inline";
+			for (let progrValue of statisticsDiv.querySelectorAll(".progress-value")) {
+				setTimeout(() => (progrValue.style.width = progrValue.getAttribute("data-value") + "%"), 1);
+			}
+		} else {
+			x.style.display = "none";
+			for (let progrValue of statisticsDiv.querySelectorAll(".progress-value")) {
+				progrValue.style.width = 0;
+			}
+		}
+	};
+	nodeTweet.appendChild(createButton);
+	tweetTextElement.appendChild(statisticsDiv);
+
+	var esp = document.createElement("span");
+	esp.id = id_comment + "pred";
+	esp.innerHTML = prediction;
+
+	let mod = document.getElementById(id_comment);
+	mod.onclick = function (event) {
+		const pred = document.getElementById(event.target.id + "pred");
+
+		const v = document.getElementById(event.target.id);
+
+		if (mod.style.filter == "blur(0px)") {
+			mod.style.filter = "blur(5px)";
+		} else {
+			mod.style.filter = "blur(0px)";
+		}
+	};
 }
 
 function get_prediction(id_comment, text_comment, callback) {
 	if (!(id_comment in comments_predictions)) {
-		// list_comments.push(text_comment);
-		// list_id.push(id_comment);
-		// console.log(text_comment);
 		const startTime = Date.now();
-		console.log("Sending request for tweet", id_comment, text_comment);
 		comments_predictions[id_comment] = [];
 		chrome.runtime.sendMessage(
 			{
@@ -61,59 +140,50 @@ function get_prediction(id_comment, text_comment, callback) {
 				url: link,
 			},
 			function (response) {
-				console.log(JSON.stringify(response));
+				//console.log(JSON.stringify(response));
 				comments_predictions[id_comment] = response;
 				const responseTime = Date.now();
-				console.log("Got response from server after:", responseTime - startTime);
-				callback(response);
+				callback(response, true);
 			}
 		);
 	} else {
 		if (comments_predictions[id_comment] != []) {
-			callback(comments_predictions[id_comment]);
+			callback(comments_predictions[id_comment], false);
 		}
 	}
 }
 
 function process_comment(element_comment, id_comment, text_comment, tweetTextElement) {
-	get_prediction(id_comment, text_comment, (prediction) => {
-		const hide = prediction["MLP_with_Roberta"][0] === 1.0; // fai cose per capire se nasconderlo
-		if (hide) {
-			hide_comment(element_comment);
+	get_prediction(id_comment, text_comment, (prediction, bool) => {
+		const hide = prediction["MLP_with_Roberta"][0] + prediction["logisticRegression"][0] + prediction["svc_bert"][0] * 0.5; // fai cose per capire se nasconderlo
+		if (hide > 1.5) {
+			hide_comment(element_comment, tweetTextElement, bool, id_comment, prediction);
 		} else {
-			tweetTextElement.style.filter = "blur(0px)";
+			//tweetTextElement.style.filter = "blur(0px)";
+			document.getElementById(id_comment).style.filter = "blur(0px)";
 		}
 	});
 }
 
 function retrieveComments(mutations, observer) {
 	for (let mutation of mutations) {
-		// console.log("Found new mutation:", mutation);
 		for (let node of mutation.addedNodes) {
-			const tweetTextElement = node.querySelector('[data-testid="tweetText"]');
-			if (node.getAttribute("data-testid") === "cellInnerDiv" && tweetTextElement) {
-				tweetTextElement.style.filter = "blur(20px)";
-				const tweetLink = node.querySelector("a:has(time)").href;
-				const tweetID = tweetLink.split("/").slice(-1)[0];
-				const tweetText = tweetTextElement.textContent;
-				console.log("Trovato tweet", tweetID, tweetText);
+			if (node.querySelector) {
+				const tweetTextElement = node.querySelector('[data-testid="tweetText"]');
+				if (node.getAttribute("data-testid") === "cellInnerDiv" && tweetTextElement) {
+					console.log(tweetTextElement.getElementsByTagName("span"));
+					tweetTextElement.getElementsByTagName("span")[0].style.filter = "blur(20px)";
+					const tweetLink = node.querySelector("a:has(time)").href;
+					const tweetID = tweetLink.split("/").slice(-1)[0];
+					tweetTextElement.getElementsByTagName("span")[0].id = tweetID;
+					const tweetText = tweetTextElement.textContent;
+					console.log("Trovato tweet", tweetID, tweetText);
 
-				process_comment(node, tweetID, tweetText, tweetTextElement);
+					process_comment(node, tweetID, tweetText, tweetTextElement, true);
+				}
 			}
-			// if (node.getAttribute("data-testid") == "tweetText") {
-			//   console.log("Trovato commento:", node);
-			// }
 		}
 	}
-
-	// var comments_retr = document.querySelectorAll('[data-testid="tweetText"]');
-	// for (var i = 0; i < comments_retr.length; i++) {
-	//   var comment = comments_retr[i];
-	//   // add to list if there are new comments
-	//   process_comment(comment);
-	// }
-
-	//console.log(comments_retr);
 }
 
 window.onload = addObserverOnTarget;
